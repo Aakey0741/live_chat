193749e4aee8a911202cd04e6175d34bc4da6448
Aakey0741 <aakash.dhingrolia@redvisiontech.com>
2022-08-02 18:47:50 +0530

    Second Push

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app =express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+     res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+     console.log('A user connected...!')
+     socket.on('chat message', (msg) => {
+          io.emit('chat message', msg)
+     })
+     socket.on('disconnect', () => {
+          console.log('User disconnected...!')
+     })
+})
+
+server.listen(1800, () => {
+     console.log(`http://localhost:${1800}/`)
+})
